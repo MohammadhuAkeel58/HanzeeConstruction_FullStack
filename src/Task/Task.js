57f8175e92b0ccbff1ebf64c2d6ab60e739e9391
Mohamed Akeel <akeelaslamcse@gmail.com>
2024-08-31 00:06:36 +0530

    made  changes

--- src/Task/Task.js
+++ src/Task/Task.js
@@ -1,89 +1,85 @@
-import { useState } from 'react';
-import TaskItem from '../Task/TaskItem';
-import EditTask from '../Task/EditTask';
+import { useState } from "react";
+import TaskItem from "../Task/TaskItem";
+import EditTask from "../Task/EditTask";
 import { doc, updateDoc, deleteDoc } from "firebase/firestore";
-import { db } from '../Config/firebase';
-import MakeSure from '../Components/MakeSure';
-import { RiUser3Fill } from "react-icons/ri";
-
+import { db } from "../Config/firebase";
+import MakeSure from "../Components/MakeSure";
 
 function Task({ id, title, description, completed }) {
-    const [checked, setChecked] = useState(completed);
-    const [open, setOpen] = useState({ edit: false, view: false });
-    const [popup, setPopup] = useState(null);
+  const [open, setOpen] = useState({ edit: false, view: false });
+  const [popup, setPopup] = useState(null);
 
-    const handleClose = () => {
-        setOpen({ edit: false, view: false });
+  const handleClose = () => {
+    setOpen({ edit: false, view: false });
+  };
+
+  const handleDelete = async () => {
+    const taskDocRef = doc(db, "tasks", id);
+    try {
+      await deleteDoc(taskDocRef);
+    } catch (err) {
+      alert(err);
     }
+  };
 
-    const handleCheckedChange = async () => {
-        const taskDocRef = doc(db, 'tasks', id);
-        try {
-            await updateDoc(taskDocRef, { completed: checked });
-        } catch (err) {
-            alert(err);
-        }
-    }
+  const handlePopup = () => {
+    setPopup(!popup);
+  };
 
-    const handleDelete = async () => {
-        const taskDocRef = doc(db, 'tasks', id);
-        try {
-            await deleteDoc(taskDocRef);
-        } catch (err) {
-            alert(err);
-        }
-    }
-
-    const handlePopup = () => {
-        setPopup(!popup);
-    }
-
-    return (
-        <div className='rounded shadow-md border-[10px] flex w-[15rem] h-[18rem] pt-12'>
-            <div className='flex w-full h-full flex-wrap'>
-                <div className='w-full h-full flex flex-col justify-evenly items-center'>
-                    
-                    <div>
-                        <p className="text-[1.2rem] font-xl capitalize">
-                            Title: {title}
-                        </p>
-                        <p className="text-[1.2rem] font-xl capitalize">
-                            Description: {description}
-                        </p>
-                    </div>
-                    <div className='flex w-full justify-evenly'>
-                        <button className='btn-1' onClick={() => setOpen({ ...open, view: true })}>
-                            View
-                        </button>
-                        <button className='btn-1 bg-light-gray' onClick={() => setOpen({ ...open, edit: true })}>
-                            Edit
-                        </button>
-                        <button className='btn-1 bg-yellow text-black' onClick={handlePopup}>
-                            Delete
-                        </button>
-                    </div>
-                </div>
-            </div>
-            {open.view && (
-                <TaskItem
-                    onClose={handleClose}
-                    title={title}
-                    description={description}
-                    open={open.view}
-                />
-            )}
-            {open.edit && (
-                <EditTask
-                    onClose={handleClose}
-                    toEditTitle={title}
-                    toEditDescription={description}
-                    open={open.edit}
-                    id={id}
-                />
-            )}
-            {popup && <MakeSure handleDelete={handleDelete} handlePopup={handlePopup} />}
+  return (
+    <div className="rounded shadow-md border-[10px] flex w-[15rem] h-[18rem] pt-12">
+      <div className="flex w-full h-full flex-wrap">
+        <div className="w-full h-full flex flex-col justify-evenly items-center">
+          <div>
+            <p className="text-[1.2rem] font-xl capitalize">Title: {title}</p>
+            <p className="text-[1.2rem] font-xl capitalize">
+              Description: {description}
+            </p>
+          </div>
+          <div className="flex w-full justify-evenly">
+            <button
+              className="btn-1"
+              onClick={() => setOpen({ ...open, view: true })}
+            >
+              View
+            </button>
+            <button
+              className="btn-1 bg-light-gray"
+              onClick={() => setOpen({ ...open, edit: true })}
+            >
+              Edit
+            </button>
+            <button
+              className="btn-1 bg-yellow text-black"
+              onClick={handlePopup}
+            >
+              Delete
+            </button>
+          </div>
         </div>
-    );
+      </div>
+      {open.view && (
+        <TaskItem
+          onClose={handleClose}
+          title={title}
+          description={description}
+          open={open.view}
+        />
+      )}
+      {open.edit && (
+        <EditTask
+          onClose={handleClose}
+          toEditTitle={title}
+          toEditDescription={description}
+          open={open.edit}
+          id={id}
+        />
+      )}
+      {popup && (
+        <MakeSure handleDelete={handleDelete} handlePopup={handlePopup} />
+      )}
+    </div>
+  );
 }
 
 export default Task;
